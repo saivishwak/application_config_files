5fd4989afeb3c8fca56aba5788270255750f3fd0
saivishwak <saivishwak40@gmail.com>
2023-04-04 07:39:19 +0530

    use rustfmt from lsp

--- navChad/custom/lspconfig.lua
+++ navChad/custom/lspconfig.lua
@@ -6,8 +6,22 @@ local lspconfig = require "lspconfig"
 local servers = {"html", "rust_analyzer", "tsserver"}
 
 for _, lsp in ipairs(servers) do
+  if lsp == 'rust_analyzer' then
   lspconfig[lsp].setup {
     on_attach = on_attach,
     capabilities = capabilities,
+    settings = {
+        ["rust-analyzer"] = {
+          checkOnSave = {
+            command = "clippy",
+          },
+        }
+    }
   }
+else
+lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+}
+  end
 end
